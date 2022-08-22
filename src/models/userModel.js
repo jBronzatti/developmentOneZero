bad123e1ebedb0b1d0dc6c8e52cc4c46b99af40a
unknown <joaovictorbronzatti@gmail.com>
2022-08-22 18:51:36 -0300

    Adicionando o modelo de usuários
    Criado modelo de usuários com: nome, email e idade.

---
+++ src/models/userModel.js
@@ -0,0 +1,29 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/database');
+
+class User extends Model {}
+
+User.init({
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    idade: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+            min:18
+        }
+    }
+},
+{
+    sequelize,
+    modelName: "usuário"
+});
+
+module.exports = User;
