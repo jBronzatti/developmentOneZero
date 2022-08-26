0c6b4d4e5a0e36276c0d95854cb070a0b6b72d72
unknown <joaovictorbronzatti@gmail.com>
2022-08-26 17:07:19 -0300

    Add isInt in idade, isEmail in email and notEmpty in all properties

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,29 +1,36 @@
 const { DataTypes, Model } = require('sequelize');
 const sequelize = require('../config/database');
 
-class User extends Model {}
+class User extends Model { }
 
 User.init({
     nome: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        notEmpty: true
     },
     email: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique: true
+        unique: true,
+        validate: {
+            isEmail: true,
+            notEmpty: true
+        }
     },
     idade: {
         type: DataTypes.INTEGER,
         allowNull: false,
         validate: {
-            min:18
+            min: 18,
+            isInt: true,
+            notEmpty: true
         }
     }
 },
-{
-    sequelize,
-    modelName: "Usuário"
-});
+    {
+        sequelize,
+        modelName: "Usuário"
+    });
 
 module.exports = User;
