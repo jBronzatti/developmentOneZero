0c03e35a5c1cb9c070e8518c80fc8bc5ce1abe56
unknown <joaovictorbronzatti@gmail.com>
2022-08-26 17:08:49 -0300

    Add logging: false in sequelize config

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,8 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize({
     dialect: "sqlite",
-    host: './database.sqlite'
+    host: './database.sqlite',
+    logging: false
 });
 
 sequelize.sync().then(() => console.log("Conectado ao banco de dados!"));
